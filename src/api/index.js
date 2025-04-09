ae7a090e2f755135e9d3aa7656b0a62ffd5a73e5
Yaroslav <jaroslav.iliuk@gmail.com>
2025-04-09 18:13:33 +0300

    fix

--- src/api/index.js
+++ src/api/index.js
@@ -53,14 +53,14 @@ const main = async () => {
   // await mainSummorize();
 };
 
-cron.schedule("1,2,4,5 * * * *", () => {
-  console.log("running every minute 1, 2, 4 and 5");
-});
-
-cron.schedule("* * * * *", () => {
-  console.log("running a task every minute");
-});
-
-// cron.schedule("0 6 * * *", () => {
-//   main();
+// cron.schedule("1,2,4,5 * * * *", () => {
+//   console.log("running every minute 1, 2, 4 and 5");
 // });
+
+// cron.schedule("* * * * *", () => {
+//   console.log("running a task every minute");
+// });
+
+cron.schedule("0 6 * * *", () => {
+  main();
+});
